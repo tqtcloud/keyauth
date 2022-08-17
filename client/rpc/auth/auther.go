b305ea30836c2ce63c4cc47368950759103ac0d8
tqtcloud <tangtao475@gmail.com>
2022-08-18 00:04:00 +0800

    auth http 中间件

---
+++ client/rpc/auth/auther.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"github.com/infraboard/mcube/logger"
+	"github.com/infraboard/mcube/logger/zap"
+	"github/tqtcloud/keyauth/apps/token"
+	"github/tqtcloud/keyauth/client/rpc"
+)
+
+func NewKeyauthAuther(client *rpc.ClientSet, serviceName string) *KeyauthAuther {
+	return &KeyauthAuther{
+		auth:        client.Token(),
+		log:         zap.L().Named("http.auther"),
+		serviceName: serviceName,
+	}
+}
+
+// 有Keyauth提供的 HTTP认证中间件
+type KeyauthAuther struct {
+	log         logger.Logger
+	auth        token.ServiceClient
+	serviceName string
+}
